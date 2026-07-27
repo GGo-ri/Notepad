a78e7361238fab926780208720a4af4c77e810f8
GGo-ri <speedmult@naver.com>
2026-07-27 09:44:11 +0900

    입력창 추가

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ menu_1.add_command(label = "저장")
 menu_1.add_separator()
 menu_1.add_command(label = "종료", command = window.destroy)
 menu.add_cascade(label = "파일", menu = menu_1)
+
 menu_2 = Menu(menu, tearoff = 0)
 menu_2.add_command(label = "개발자")
 menu.add_cascade(label = "개발자", menu = menu_2)
+
+text_area = Text(window)
+
+window.grid_rowconfigure(0, weight = 1)
+window.grid_columnconfigure(0, weight = 1)
+
+text_area.grid(sticky = N + E + S + W)
 
 window.config(menu = menu)
 window.mainloop()
